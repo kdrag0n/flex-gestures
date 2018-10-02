62c83c93c21f5d15307fc78f863f17e19bd285b2
kdrag0n <dragon@khronodragon.com>
2018-10-01 19:48:03 -0700

    PrivateWindowManager: convert to singleton object

--- app/src/main/java/com/kdrag0n/flexgestures/utils/PrivateWindowManager.kt
+++ app/src/main/java/com/kdrag0n/flexgestures/utils/PrivateWindowManager.kt
@@ -4,21 +4,19 @@ import android.annotation.SuppressLint
 import java.lang.reflect.Method
 
 @SuppressLint("PrivateApi")
-class PrivateWindowManager {
-    companion object {
-        private val int = Int::class.java
-        private val service = Class.forName("android.view.WindowManagerGlobal")
-                .getMethod("getWindowManagerService")
-                .invoke(null)
-        private val wmClass: Class<*> = Class.forName("android.view.IWindowManager")
+object PrivateWindowManager {
+    private val int = Int::class.java
+    private val service = Class.forName("android.view.WindowManagerGlobal")
+            .getMethod("getWindowManagerService")
+            .invoke(null)
+    private val wmClass: Class<*> = Class.forName("android.view.IWindowManager")
 
-        private fun <T: Any> getMethod(name: String, vararg types: Class<T>): Method {
-            return wmClass.getMethod(name, *types)
-        }
+    private fun <T: Any> getMethod(name: String, vararg types: Class<T>): Method {
+        return wmClass.getMethod(name, *types)
+    }
 
-        fun setOverscan(display: Int, left: Int, top: Int, right: Int, bottom: Int) {
-            getMethod("setOverscan", int, int, int, int, int)
-                    .invoke(service, display, left, top, right, bottom)
-        }
+    fun setOverscan(display: Int, left: Int, top: Int, right: Int, bottom: Int) {
+        getMethod("setOverscan", int, int, int, int, int)
+                .invoke(service, display, left, top, right, bottom)
     }
 }
